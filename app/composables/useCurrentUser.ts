55fafe56d8d70382fbe1310e2f54bb4f73bdb3e6
Grady Link <github@grady.link>
2025-09-04 23:15:14 -0700

    fix(auth): function when logged out
    auth is so finicky

--- app/composables/useCurrentUser.ts
+++ app/composables/useCurrentUser.ts
@@ -1,14 +1,18 @@
 export const useCurrentUser = async () => {
-  const { data: user } = await useAsyncData(
-    "user",
-    () =>
-      $fetch("/api/auth/me", {
-        headers: useRequestHeaders(["cookie"]),
-      }).catch(() => null),
-  );
+  try {
+    const { data: user } = await useAsyncData(
+      "user",
+      () =>
+        $fetch("/api/auth/me", {
+          headers: useRequestHeaders(["cookie"]),
+        }).catch(() => null),
+    );
 
-  return {
-    loggedIn: !!user.value?.user,
-    username: (user.value?.user as { username: string }).username || "",
-  };
+    return {
+      loggedIn: !!user.value?.user,
+      username: (user.value?.user as { username: string }).username || "",
+    };
+  } catch {
+    return { loggedIn: false, username: "" };
+  }
 };

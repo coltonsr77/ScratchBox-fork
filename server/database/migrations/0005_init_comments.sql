4ddda2210cc926b9ffdaa92e3c7aa10197b05c5d
Grady Link <github@grady.link>
2025-09-12 20:29:40 -0700

    fix: correct comments migration
    wtf drizzle, literally ur only job

--- server/database/migrations/0005_init_comments.sql
+++ server/database/migrations/0005_init_comments.sql
@@ -6,26 +6,3 @@ CREATE TABLE `project_comments` (
 	`created_at` integer NOT NULL,
 	FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`) ON UPDATE no action ON DELETE no action
 );
---> statement-breakpoint
-CREATE TABLE `project_likes` (
-	`project_id` text NOT NULL,
-	`user` text NOT NULL,
-	PRIMARY KEY(`project_id`, `user`),
-	FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
-CREATE TABLE `project_platforms` (
-	`project_id` text NOT NULL,
-	`platform` text NOT NULL,
-	PRIMARY KEY(`project_id`, `platform`),
-	FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
-CREATE TABLE `projects` (
-	`id` text PRIMARY KEY NOT NULL,
-	`name` text NOT NULL,
-	`description` text NOT NULL,
-	`created_at` integer NOT NULL,
-	`private` integer NOT NULL,
-	`user` text NOT NULL
-);

1235eb0559cc4c00c73f34e09c06c340c6c4084b
Grady Link <github@grady.link>
2025-10-11 18:36:19 -0700

    fix: another attempt

--- server/database/migrations/0006_init_unistore.sql
+++ server/database/migrations/0006_init_unistore.sql
@@ -4,4 +4,6 @@ CREATE TABLE `unistore_data` (
 --> statement-breakpoint
 INSERT INTO `unistore_data` VALUES(0)
 --> statement-breakpoint
-ALTER TABLE `projects` ADD `last_updated` integer NOT NULL DEFAULT (strftime('%s', 'now'));
+ALTER TABLE `projects` ADD `last_updated` integer;
+--> statement-breakpoint
+UPDATE `projects` SET `last_updated` = strftime('%s', 'now') WHERE `last_updated` IS NULL;
